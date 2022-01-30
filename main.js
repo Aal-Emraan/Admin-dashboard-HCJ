b55e9ac981bae1625e25e3d908f649967892d3a0
Aal <developer.aalemraan@gmail.com>
2022-01-30 22:58:50 +0600

    javascript sidebar and darkmode done

--- main.js
+++ main.js
@@ -41,3 +41,28 @@ new Chart(chart, {
         responsive: "true",
     },
 });
+
+// Show or Hide Sidebar
+
+const menubtn = document.querySelector("#menu-btn");
+const closebtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("aside");
+
+menubtn.addEventListener("click", () => {
+    sidebar.style.display = "block";
+});
+
+closebtn.addEventListener("click", () => {
+    sidebar.style.display = "none";
+});
+
+// Dark theme button
+
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+
+    themeBtn.querySelector("span:first-child").classList.toggle("active");
+    themeBtn.querySelector("span:last-child").classList.toggle("active");
+});
